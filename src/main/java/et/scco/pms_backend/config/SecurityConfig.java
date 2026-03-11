d55cc5f4545e62a79f2126731f4c772803e6aa2c
Bayo-Nike <bayisa@siinqee.com>
2026-03-11 18:02:39 +0300

    Color coding backend updated

--- src/main/java/et/scco/pms_backend/config/SecurityConfig.java
+++ src/main/java/et/scco/pms_backend/config/SecurityConfig.java
@@ -45,9 +45,8 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/login").permitAll()
                         .requestMatchers("/api/auth/profile").authenticated()
                         .requestMatchers("/api/admin/contractors/download/**").permitAll() // allow contractor file view/download
-                        // .requestMatchers("/api/admin/**").hasRole("SUPER_ADMIN")
-                        .requestMatchers("/api/admin/**").hasAnyRole("SUPER_ADMIN","CITY_MAYOR")
-                        .requestMatchers("/api/colorCodes/**").hasAnyRole("SUPER_ADMIN","CITY_MAYOR")
+                        .requestMatchers("/api/admin/**").hasRole("SUPER_ADMIN")
+                        .requestMatchers("/api/colorCodes/**").hasRole("SUPER_ADMIN")
                         .anyRequest().authenticated()
                 );
 
